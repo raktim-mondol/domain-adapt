361ae249b673ceb69a155750824584cb0b7905b1
Raktim Kumar Mondol <raktim.live@gmail.com>
2025-11-12 01:27:58 +1100

    Add files via upload

---
+++ classification_model/src/models/__init__.py
@@ -0,0 +1,13 @@
+"""
+Neural Network Models for BMA Classification
+"""
+
+from .bma_mil_model import (
+    BMA_MIL_Classifier,
+    AttentionAggregator
+)
+
+__all__ = [
+    'BMA_MIL_Classifier',
+    'AttentionAggregator'
+]
